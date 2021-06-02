3e3f4754000296fbf1eb1c2c6011ce3508b99b44
Matvey <mr_matvey444@mail.ru>
2021-06-02 17:50:31 +0300

    UPD-0.0.4

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -13,7 +13,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     version=version,
-    packages=find_packages(),
+    packages=["vkuserbot"],
     install_requires=[
         "aiohttp",
         "aiosqlite",
